af275b09764cdb6e9d0d7b75bea1557c45e2f60c
Rachel Loziuk <rachel@reviewpush.com>
2021-07-26 13:34:01 -0700

    create a ClimateData controller

---
+++ app/Http/Controllers/ClimateDataController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClimateDataController extends Controller
+{
+    public function index()
+    {
+        dd("Climate data results");
+    }
+}
