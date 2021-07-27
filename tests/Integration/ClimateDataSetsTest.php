9ab688a38d6945b9cb1674b8a4694814fb9c1f5b
Rachel Loziuk <rachel@reviewpush.com>
2021-07-27 11:52:50 -0700

    outline tests for climate data

---
+++ tests/Integration/ClimateDataSetsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Integration\Api;
+
+class ClimateDataSetsTests
+{
+    /**
+     * Expect a climate dataset: moon hourly position, sun hourly position, tide hourly level, and moon phase.
+     * When I submit a GET request to the external weather API with a particular DATE and LOCATION
+     *
+     * @return void
+     */
+   public function test_climate_dataset_for_given_date_and_location()
+   {
+       // TODO
+   }
+}
