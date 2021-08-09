14c04c79d3ff6e4c55f91eabaf4e71b935827546
Rachel Loziuk <rachel@reviewpush.com>
2021-08-09 16:16:21 -0700

    create a contract for the climate service classes.

---
+++ app/ThirdPartyApi/ClimateServiceContract.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\ThirdPartyApi;
+
+interface ClimateServiceContract
+{
+    public function moon_rise();
+    public function moon_set();
+    public function sun_rise();
+    public function sun_set();
+    public function moon_phase();
+    public function high_tides();
+    public function low_tides();
+}
