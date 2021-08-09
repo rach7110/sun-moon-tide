1979682d48fb83b22024517988e231602abec4fa
Rachel Loziuk <rachel@reviewpush.com>
2021-08-09 16:15:32 -0700

    create a contract for the external api providers.

---
+++ app/ThirdPartyApi/ClimateProviderContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\ThirdPartyApi;
+
+interface ClimateProviderContract
+{
+    public function set_climate_data();
+
+    public function get_climate_data();
+
+    public function fetch();
+}
