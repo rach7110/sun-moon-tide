13902f8b5190889c39f7558bbbff889b4cfd27d3
Rachel Loziuk <rachel@reviewpush.com>
2021-08-10 16:39:26 -0700

    bind the Solunar class to the ClimateProviderContact.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ClimateProviderContract::class, Solunar::class);
     }
 
     /**
