150a83cf8bb8dc99262cd8206a32e1c8a733794b
Rachel Loziuk <rachel@reviewpush.com>
2021-08-10 16:34:12 -0700

    add additional api keys to config file.

--- config/climate.php
+++ config/climate.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
-    'climate_url' => 'https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets',
-    'climate_token' => env('NOAA_NCDC_TOKEN', null),
+    'noaa' => [
+        'base_url' => 'https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets',
+        'token' => env('NOAA_NCDC_TOKEN', null)
+    ],
+    'solunar' => [
+        'base_url' => 'https://api.solunar.org/solunar',
+    ]
 ];
