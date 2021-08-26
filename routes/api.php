7beb1fe2134a1e44c3c6596acaa1796f2b658166
Rachel Loziuk <rachel@reviewpush.com>
2021-08-26 11:13:25 -0700

    use resource naming conventions.

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/climate', [ClimateDatasetsController::class, 'show']);
+Route::get('/climate/create', [ClimateDatasetsController::class, 'create']);
+Route::post('/climate', [ClimateDatasetsController::class, 'fetch']);
