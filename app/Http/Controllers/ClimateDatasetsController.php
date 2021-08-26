0af8b820fe146343b4b4336852a0c0c901eae71c
Rachel Loziuk <rachel@reviewpush.com>
2021-08-26 13:28:50 -0700

    WIP: dataset controller fetch method.

--- app/Http/Controllers/ClimateDatasetsController.php
+++ app/Http/Controllers/ClimateDatasetsController.php
@@ -9,29 +9,42 @@ use Illuminate\Http\Request;
 
 class ClimateDatasetsController extends Controller
 {
-    public function show(Solunar $provider, ClimateServiceContract $climate_svc)
+
+    public function create()
     {
+        // TODO: create view for form inputs.
+    }
 
-        // TODO: get date from query parameters and parse.
-        // TODO: get location from query parameters and parse.
+    public function fetch(Request $request, Solunar $provider, ClimateServiceContract $climate_svc)
+    {
+        $validated = $request->validate([
+            'date' => 'required|date_format:d-m-Y',
+            'zip' =>'required'
+        ]);
 
-        $provider->set_date("20210801");
-        $provider->set_location("42.66,-84.07");
-        $provider->set_timezone("-5");
+        if ($validated) {
+            $provider->set_date($request->input('date'));
 
-        $data = $provider->fetch();
+            // TODO: use a default timezone if one is not provided.
+            $provider->set_timezone("-11");
 
-        $climate_svc->set_data($data);
+            $provider->set_location("42.66,-84.07");
 
-        $climate_dataset = [
-            'moon_rise' => $climate_svc->moon_rise(),
-            'moon_set' => $climate_svc->moon_set(),
-            'sun_rise' => $climate_svc->sun_rise(),
-            'sun_set' => $climate_svc->sun_set(),
-            'tides_high' => $climate_svc->high_tides(),
-            'tides_low' => $climate_svc->low_tides(),
-            'moon_phase' => $climate_svc->moon_phase()
-        ];
+            $data = $provider->fetch();
+
+            $climate_svc->set_data($data);
+
+            $climate_dataset = [
+                'moon_rise' => $climate_svc->moon_rise(),
+                'moon_set' => $climate_svc->moon_set(),
+                'sun_rise' => $climate_svc->sun_rise(),
+                'sun_set' => $climate_svc->sun_set(),
+                'tides_high' => $climate_svc->high_tides(),
+                'tides_low' => $climate_svc->low_tides(),
+                'moon_phase' => $climate_svc->moon_phase()
+            ];
+        }
+        // TODO: handle validation errors.
 
         // TODO: remove transformer. Since we are not accessing the data in multiple places in our application, all the transformation may happen in the controller.
 
