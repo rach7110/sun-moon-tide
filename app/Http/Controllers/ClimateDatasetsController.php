7bdbd796a42c4ed057ffa7bf061d2bdeb2e3fc2a
Rachel Loziuk <rachel@reviewpush.com>
2021-11-04 16:52:36 -0500

    remove unused import.

--- app/Http/Controllers/ClimateDatasetsController.php
+++ app/Http/Controllers/ClimateDatasetsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\ClimateServiceContract;
-use App\Service\Solunar\SolunarService;
 use App\Transformers\ClimateDatasetTransformer;
 use Exception;
 use Illuminate\Http\Request;
