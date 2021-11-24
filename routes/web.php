690d608db398a93b319fdf1e926bd110ad02dd02
Rachel Loziuk <rachel@reviewpush.com>
2021-11-24 17:55:53 -0600

    include debugbar package.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/climate/create', function() {
     return view('climatedata');
 })->name('climatedata');
+
+Route::post('/climate/create', function() {
+    return "Hello World";
+});
