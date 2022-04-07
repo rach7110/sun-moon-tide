ff5f294af272ae3f3b9faaf4f7d3d45f321f4c68
Rachel Loziuk <rachel@reviewpush.com>
2022-04-07 15:44:46 -0500

    WIP: NoaaTideService parsing of api response.

--- app/Service/Tide/NoaaTideService.php
+++ app/Service/Tide/NoaaTideService.php
@@ -6,6 +6,7 @@ use App\Contracts\TideServiceContract;
 use App\ThirdPartyApi\NoaaTides;
 use App\Traits\FormatsInput;
 use App\Traits\ValidatesInput;
+use Carbon\Carbon;
 use Exception;
 
 class NoaaTideService extends TideServiceContract
@@ -33,13 +34,16 @@ class NoaaTideService extends TideServiceContract
         $dataset = [];
 
         // Validation rules.
-        try {
-            $this->validate($inputs);
-        } catch (Exception $e) {
-            print_r($e->getMessage()); // TODO: handle
-        }
+        //TODO
+        // try {
+        //     $this->validate($inputs);
+        // } catch (Exception $e) {
+        //     print_r($e->getMessage()); // TODO: handle
+        // }
 
-        $formatted = $this->format_inputs($inputs);
+        // TODO
+        // $formatted = $this->format_inputs($inputs);
+        $formatted = $inputs;
 
         $dataset = $this->provider->fetch(
             $formatted['date'],
@@ -53,30 +57,60 @@ class NoaaTideService extends TideServiceContract
     /**
      * Parse the values from the external api.
      *
-     * @param Object $api_response
+     * @param Object $response
      * @return void
      */
-    //TODO does it accept an Object or string?
-    public function parse($api_response) {
-        $this->high_tides = $this->extract_high_tides($api_response);
-        $this->low_tides =  $this->extract_low_tides($api_response);
+    public function parse($response) {
+        $this->high_tides = $this->extract_high_tides($response);
+        $this->low_tides =  $this->extract_low_tides($response);
     }
 
     /**
      * Filters out the high tide data from the api response
+     * and sets it as a class property.
      *
-     * @return void
+     * @param Object $response
+     * @return array $high_tides
      */
-    private function extract_high_tides()
-    {}
+    private function extract_high_tides($response)
+    {
+        $high_tides = [];
+        $data = collect($response->data);
+
+        // Get half of the day's values. (There are 240 x 6 minute segments in a day.
+        $chunks = $data->chunk(120);
+        $data1 = $chunks[0];
+        $data2 = $chunks[1];
+
+        // Get the max tides from each half of the day
+        // BUG this won't work for days when there is only 1 high tide.
+        // Better implementation is to find the maxes in each half. Then ensure the values just before and after are less than the max. If not, disregard the max as it is not an apex.
+        $max1 = $data1->max('v');
+        $max2 = $data2->max('v');
+        $max_dataset1 = $data1->where('v', $max1);
+        $max_dataset2 = $data2->where('v', $max2);
+        $first_high_tide = (collect($max_dataset1->first())->get('t'));
+        $second_high_tide = (collect($max_dataset2->first())->get('t'));
+
+        // Get time of high tides in the format 'hh:mm'
+        $first_high_tide = Carbon::createFromFormat('Y-m-d H:i', $first_high_tide)->toTimeString('minute');
+        $second_high_tide = Carbon::createFromFormat('Y-m-d H:i', $second_high_tide)->toTimeString('minute');
+
+        array_push($high_tides, $first_high_tide, $second_high_tide);
+
+        return $high_tides;
+    }
 
     /**
      * Filters out the low tide data from the api response
+     * and sets it as a class property.
      *
      * @return void
      */
     private function extract_low_tides()
-    {}
+    {
+        // TODO
+    }
 
     public function get_high_tides() {
         return $this->high_tides;
--- tests/Unit/Tide/NoaaTideServiceTest.php
+++ tests/Unit/Tide/NoaaTideServiceTest.php
@@ -5,28 +5,26 @@ namespace Test\Unit\Tide;
 use App\Service\Tide\NoaaTideService;
 use Tests\Unit\Tide\NoaaTideServiceBaseCase;
 
-/**
- * Tests exceptions are thrown for wrong inputs, tests formats inputs correctly, tests returned object is formatted correctly.
- */
 class NoaaTideServiceTest extends NoaaTideServiceBaseCase
 {
     public function setup(): void
     {
+        parent::setUp();
+
         $this->tide_svc = new NoaaTideService;
-
-        $this->tide_svc->set_data($this->api_response);
-
-        //  TODO set api data on service class
+        $this->tide_svc->parse($this->api_response);
     }
 
 
 
-    public function test_gets_moon_rise()
+    public function test_gets_high_tides()
     {
     // TODO
+    $high_tides = ['04:36','15:00'];
+        $this->assertEquals($high_tides, $this->tide_svc->get_high_tides());
     }
 
-    public function test_gets_moon_set()
+    public function test_gets_low_tides()
     {
     // TODO
     }
