ab602856e041485b2268f21e9ec46a2d29a93fbd
Rachel Loziuk <rachel@reviewpush.com>
2022-04-15 10:04:44 -0500

    update application parameter in api url.

--- config/climate.php
+++ config/climate.php
@@ -6,7 +6,7 @@ return [
         'token' => env('NOAA_NCDC_TOKEN', null)
     ],
     'noaa_tides' => [
-        'base_url' => 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=water_level&units=english&application=ports_screen&format=json&datum=MSL'
+        'base_url' => 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=water_level&units=english&application=rachelL&format=json&datum=MSL'
     ],
     'solunar' => [
         'base_url' => 'https://api.solunar.org/solunar',
