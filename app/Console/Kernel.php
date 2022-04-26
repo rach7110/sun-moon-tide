ae4e42450f8cb9a98cd10eb27585001e24e08103
Rachel Loziuk <rachel@reviewpush.com>
2022-04-26 13:11:43 -0500

    add updateNoaaBuoys to a scheduled task.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\UpdateNoaaBuoys;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new UpdateNoaaBuoys)
+            ->dailyAt('9:00') // GMT or 2am Chicago timezone.
     }
 
     /**
