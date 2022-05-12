cf05f44047d480b6efc50dca57d45da36e89b2c4
Rachel Loziuk <rachel@reviewpush.com>
2022-05-11 20:59:12 -0500

    import HasApiTokens trait in user model.

--- app/Http/Controllers/ApiTokenController.php
+++ app/Http/Controllers/ApiTokenController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Laravel\Sanctum\HasApiTokens;
 
 class ApiTokenController extends Controller
 {
-    use HasApiTokens;
-
     public function create(Request $request)
     {
+        // BUG : null user.
         $token = $request->user()->createToken($request->token_name);
 
         return ['token' => $token->plainTextToken];
