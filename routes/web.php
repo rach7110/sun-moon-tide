3cbebff5edbd8521ffe3eaf503ef7455b704e558
Rachel Loziuk <rachel@reviewpush.com>
2022-05-12 13:25:27 -0500

    restore route to view climate page.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/climate/create', function() {
+    return view('climatedata');
+})->name('climatedata');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
